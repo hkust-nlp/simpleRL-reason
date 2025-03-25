dcdc0115704858464a77a4f96b71cd6cac17b686
Aidan Do <aidando73@gmail.com>
2025-03-25 21:33:39 +1100

    .

--- train_grpo_math_tune_ray.sh
+++ train_grpo_math_tune_ray.sh
@@ -174,8 +174,8 @@ ray job submit --address=${HEAD_IP}:${HEAD_PORT} \
     }' \
   -- python -m verl.trainer.main_ppo \
   algorithm.adv_estimator=grpo \
-  data.train_files=$HDFS_DATA_PATH/$DATASET_NAME/train.parquet \
-  data.val_files=$HDFS_DATA_PATH/$DATASET_NAME/test.parquet \
+  data.train_files=$HOME/simpleRL-reason/train.parquet \
+  data.val_files=$HOME/simpleRL-reason/test.parquet \
   data.train_batch_size=$TRAIN_BATCH_SIZE \
   data.val_batch_size=$VAL_BATCH_SIZE \
   data.max_prompt_length=$MAX_PROMPT_LENGTH \
